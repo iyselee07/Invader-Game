d96f1c2bdf279bc7ca72bd6e2ef05e2149d15f6c
iyselee07 <nerv.201526@gmail.com>
2017-12-01 17:58:46 +0900

    start enpling UI window

---
+++ Invader/End.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace Invader
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class End : Page
+    {
+        Stage stage;
+        public End()
+        {
+            this.InitializeComponent();
+            stage = Stage.getInstance();
+            stage.clock += Stage_clock;
+            this.Loaded += delegate { Application.Current.Exit(); };
+        }
+
+        private void keyPressed(object sender, KeyRoutedEventArgs e)
+        {
+
+        }
+
+        private void keyReleased(object sender, KeyRoutedEventArgs e)
+        {
+
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+        }
+
+        private void Stage_clock(object sender, EventArgs e)
+        {
+            //
+            //Application.Current.Exit();
+        }
+    }
+}
--- Invader/obj/x86/Debug/XamlTypeInfo.g.cs
+++ Invader/obj/x86/Debug/XamlTypeInfo.g.cs
@@ -132,15 +132,19 @@ namespace Invader.Invader_XamlTypeInfo
 
         private void InitTypeTables()
         {
-            _typeNameTable = new string[3];
-            _typeNameTable[0] = "Invader.MainPage";
+            _typeNameTable = new string[5];
+            _typeNameTable[0] = "Invader.End";
             _typeNameTable[1] = "Windows.UI.Xaml.Controls.Page";
             _typeNameTable[2] = "Windows.UI.Xaml.Controls.UserControl";
+            _typeNameTable[3] = "Invader.GameOver";
+            _typeNameTable[4] = "Invader.MainPage";
 
-            _typeTable = new global::System.Type[3];
-            _typeTable[0] = typeof(global::Invader.MainPage);
+            _typeTable = new global::System.Type[5];
+            _typeTable[0] = typeof(global::Invader.End);
             _typeTable[1] = typeof(global::Windows.UI.Xaml.Controls.Page);
             _typeTable[2] = typeof(global::Windows.UI.Xaml.Controls.UserControl);
+            _typeTable[3] = typeof(global::Invader.GameOver);
+            _typeTable[4] = typeof(global::Invader.MainPage);
         }
 
         private int LookupTypeIndexByName(string typeName)
@@ -175,7 +179,9 @@ namespace Invader.Invader_XamlTypeInfo
             return -1;
         }
 
-        private object Activate_0_MainPage() { return new global::Invader.MainPage(); }
+        private object Activate_0_End() { return new global::Invader.End(); }
+        private object Activate_3_GameOver() { return new global::Invader.GameOver(); }
+        private object Activate_4_MainPage() { return new global::Invader.MainPage(); }
 
         private global::Windows.UI.Xaml.Markup.IXamlType CreateXamlType(int typeIndex)
         {
@@ -187,9 +193,9 @@ namespace Invader.Invader_XamlTypeInfo
             switch (typeIndex)
             {
 
-            case 0:   //  Invader.MainPage
+            case 0:   //  Invader.End
                 userType = new global::Invader.Invader_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
-                userType.Activator = Activate_0_MainPage;
+                userType.Activator = Activate_0_End;
                 userType.SetIsLocalType();
                 xamlType = userType;
                 break;
@@ -200,6 +206,20 @@ namespace Invader.Invader_XamlTypeInfo
 
             case 2:   //  Windows.UI.Xaml.Controls.UserControl
                 xamlType = new global::Invader.Invader_XamlTypeInfo.XamlSystemBaseType(typeName, type);
+                break;
+
+            case 3:   //  Invader.GameOver
+                userType = new global::Invader.Invader_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
+                userType.Activator = Activate_3_GameOver;
+                userType.SetIsLocalType();
+                xamlType = userType;
+                break;
+
+            case 4:   //  Invader.MainPage
+                userType = new global::Invader.Invader_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
+                userType.Activator = Activate_4_MainPage;
+                userType.SetIsLocalType();
+                xamlType = userType;
                 break;
             }
             return xamlType;

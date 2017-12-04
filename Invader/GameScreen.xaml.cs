f96ef28a8bd8fa7815db1d9cca02d60b8423dcd9
iyselee07 <nerv.201526@gmail.com>
2017-12-04 14:20:14 +0900

    impling display GameObjects

--- Invader/GameScreen.xaml.cs
+++ Invader/GameScreen.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
 
@@ -22,9 +23,65 @@ namespace Invader
     /// </summary>
     public sealed partial class GameScreen : Page
     {
+        Stage stage;
+        Existence exist;
+        DyingExistence dying;
         public GameScreen()
         {
             this.InitializeComponent();
+            exist = Existence.getInstance();
+            dying = DyingExistence.getInstance();
+            stage = Stage.getInstance();
+            stage.clock += Stage_clock;
+            this.Loaded += delegate {
+                this.keyholder.Focus(FocusState.Keyboard);
+                this.keyholder.LostFocus += (s, e) => this.keyholder.Focus(FocusState.Keyboard);
+                this.keyholder.IsTabStop = true;
+                //Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TryResizeView(new Size { Width = Def.existWidth, Height = Def.existHeight });
+            };
         }
+
+        private async void Stage_clock(object sender, EventArgs e)
+        {
+            switch (stage.state)
+            {
+                case Def.State.GameOver:
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+                    {
+                        this.Frame.Navigate(typeof(GameOver));
+                    });
+                    break;
+                case Def.State.BeShotDown:
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+                    {
+                        this.Frame.Navigate(typeof(ShotDown));
+                    });
+                    break;
+                case Def.State.InGame:
+
+                    break;
+            }
+        }
+
+        private Task syncObjToImg()
+        {
+            
+        }
+
+        private void Grid_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            stage.interactByKeyPress(sender, e);
+        }
+
+        private void Grid_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            stage.interactByKeyRelease(sender, e);
+        }
+    }
+
+    class GameObjectImage
+    {
+        public Image displayImage { private set; get; }
+
     }
 }

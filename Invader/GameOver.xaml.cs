880c5f6e247905a95255ac23e07078447022aaa3
iyselee07 <nerv.201526@gmail.com>
2017-12-13 09:42:41 +0900

    impl things that gameover screen have to do

--- Invader/GameOver.xaml.cs
+++ Invader/GameOver.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -22,19 +23,64 @@ namespace Invader
     /// </summary>
     public sealed partial class GameOver : Page
     {
+        private Stage stage;
+        private Timer timer;
         public GameOver()
         {
+            stage = Stage.getInstance();
+            TimerCallback callback = state => { Stage_clock(); };
+            timer = new Timer(callback, null, 0, Def.frameSpan);
             this.InitializeComponent();
+            this.Loaded += delegate {
+                this.keyholder.Focus(FocusState.Keyboard);
+                this.keyholder.LostFocus += (s, e) => this.keyholder.Focus(FocusState.Keyboard);
+                this.keyholder.IsTabStop = true;
+            };
+        }
+
+        private async void Stage_clock()
+        {
+            if (stage.state == Def.State.End)
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+                {
+                    this.Frame.Navigate(typeof(End));
+                });
+            }
+            else if (stage.state != Def.State.GameOver)
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+                {
+                    timer.Change(Timeout.Infinite, Timeout.Infinite);
+                    timer.Dispose();
+                    this.keyholder.LostFocus += (s, e) => { };
+                    this.Frame.Navigate(typeof(MainPage));
+
+                });
+            }
+            else
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+                {
+                    this.keyholder.Focus(FocusState.Keyboard);
+                });
+            }
         }
 
         private void keyPressed(object sender, KeyRoutedEventArgs e)
         {
-            
+            if (stage.state == Def.State.GameOver)
+            {
+                stage.interactByKeyPress(sender, e);
+            }
         }
 
         private void keyReleased(object sender, KeyRoutedEventArgs e)
         {
-
+            if (stage.state == Def.State.GameOver)
+            {
+                stage.interactByKeyRelease(sender, e);
+            }
         }
     }
 }
